159ffb77ced2fe1609d60ad7382b27da41b32348
hereafter <tu.tong@outlook.com>
2023-09-22 16:16:56 +0800

    fix paths

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { DClass } from "./decorators/dclass";
 export { DName } from "./decorators/dname";
+export { DConvert } from "./decorators/dconverter";
 export { convert } from "./dconvert";
---
+++ tests/test.d.ts
@@ -0,0 +1,6 @@
+export declare class TestClass {
+    x: number;
+    y: number;
+    get sum(): number;
+}
+export declare function fnTestSimple(): void;
